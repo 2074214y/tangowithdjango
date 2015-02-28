83af58d643dd8801c8d311d61b7ceaafa54a7cc8
2074214y <theorouskata@gmail.com>
2015-02-28 23:18:15 +0000

    chapter 12 complete

--- code/tango_with_django_project/tango_with_django_project/urls.py
+++ code/tango_with_django_project/tango_with_django_project/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
 
-
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/rango/'
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,7 +14,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
-    url(r'^newproject/', include('newproject.urls')),
+        #Add in this url pattern to override the default pattern in accounts.
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    (r'^accounts/', include('registration.backends.simple.urls')),
     )
 
 if settings.DEBUG:
